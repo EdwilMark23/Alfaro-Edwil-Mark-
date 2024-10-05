233f65a937c7e162538cd836f818beeaf7ab0440
Cedeno Mark bryan <cedenomarkbryan04@gmail.com>
2024-10-05 19:41:14 +0800

    Organizing Structure and Adding Header and Footer

--- PHP/index.php
+++ PHP/index.php
@@ -5,31 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kampon ni Aries</title>
-    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
-    <link rel="stylesheet" href="indexstyles.css">
+    <link rel="icon" type="image/x-icon" href="../images/favicon.ico">
+    <link rel="stylesheet" href="../CSS/indexstyles.css">
     <script>
-
-        // window.onload = function userfunction() {
-        //     let number = Math.floor(Math.random() * 4); // Generates a random number between 0 and 3
-        //     let name = "Bini Maoy"; // Initialize the variable `name`
-        //     if (number === 1) {
-        //         name = "Bini log";
-        //     } else if (number === 2) {
-        //         name = "Bini Lad";
-        //     } else if (number === 3) {
-        //         name = "Bini Malou";
-        //     }
-        //     let person = prompt("Please enter your nickname lodicakes", name);
-        //     if (person != null) {
-        //         document.getElementById("moreinfo").innerHTML =
-        //         "Greetings, " + person + " We’re the Group 5, a team of six extraordinary web developers, each a master in their field, dedicated to pushing the boundaries of digital creation. From crafting pixel-perfect designs to engineering robust systems, our collective expertise transforms visions into interactive realities.";
-        //         document.getElementById("intro").innerHTML = "Sup, " + person + "! Meet Out Team";
-        //     }
-        // }
-
         function moreinfofunction() {
             document.getElementById("moreinfo").innerHTML = "Naghintay kaba wala na, it's a prank";
-            document.getElementById('cong').src='images/Prank.jfif';
+            document.getElementById('cong').src='../images/Prank.jfif';
             document.getElementById('cong').style.height = "85px";
             document.getElementById('cong').style.width = "150px";
         }
@@ -92,7 +73,7 @@
         <h1 id="intro"> Sup <?php echo $member ?>! Meet Our Team</h1>
         <div class="team-container">
             <div class="team-member">
-                <img src="images/marcelino.gif" alt="Marcelino">
+                <img src="../images/marcelino.gif" alt="Marcelino">
                 <h3>
                     <?php echo $marcelino ?>
                 </h3>
@@ -101,7 +82,7 @@
                 <a href="#marcelino-profile">View Profile</a>
             </div>
             <div class="team-member">
-                <img src="images/cedeno.jpg" alt="Cedeno">
+                <img src="../images/cedeno.jpg" alt="Cedeno">
                 <h3>
                     <?php echo $cedeno ?>
                 </h3>
@@ -110,7 +91,7 @@
                 <a href="#cedeno-profile">View Profile</a>
             </div>
             <div class="team-member">
-                <img src="images/rivas.jpg" alt="Rivas">
+                <img src="../images/rivas.jpg" alt="Rivas">
                 <h3>
                     <?php echo $rivas ?>
                 </h3>
@@ -119,7 +100,7 @@
                 <a href="#rivas-profile">View Profile</a>
             </div>
             <div class="team-member">
-                <img src="images/bunyi.jpg" alt="Bunyi">
+                <img src="../images/bunyi.jpg" alt="Bunyi">
                 <h3>
                     <?php echo $bunyi ?>
                 </h3>
@@ -128,7 +109,7 @@
                 <a href="#bunyi-profile">View Profile</a>
             </div>
             <div class="team-member">
-                <img src="images/falcon.jpg" alt="Falcon">
+                <img src="../images/falcon.jpg" alt="Falcon">
                 <h3>
                     <?php echo $falcon ?>
                 </h3>
@@ -137,7 +118,7 @@
                 <a href="#falcon-profile">View Profile</a>
             </div>
             <div class="team-member">
-                <img src="images/alfaro.jpg" alt="Alfaro">
+                <img src="../images/alfaro.jpg" alt="Alfaro">
                 <h3>
                     <?php echo $alfaro ?>
                 </h3>
@@ -151,7 +132,7 @@
                 <p id="moreinfo">
                     Welcome to the Group 5 <?php echo $member ?>, we are a team of extraordinary web developers, each a master in their field, dedicated to pushing the boundaries of digital creation. From crafting pixel-perfect designs to engineering robust systems, our collective expertise transforms visions into interactive realities.
                 </p>
-                <img id="cong" src="images/quotation.jfif">
+                <img id="cong" src="../images/quotation.jfif">
                 <div>
                     <button class="more-info" type="button" onclick="setTimeout(moreinfofunction, 3000)">Click for more team info</button>
                 </div>
@@ -161,7 +142,7 @@
     <section class="profiles">
         <div id="marcelino-profile" class="profile">
             <div class="profile-header">
-                <img src="images/marcelino.gif" alt="Geminaries Marcelino">
+                <img src="../images/marcelino.gif" alt="Geminaries Marcelino">
                 <h2>
                     <?php echo $marcelino ?>
                 </h2>
@@ -172,7 +153,7 @@
                 <p> <?php echo "22 years old" ?> </p>
                 <p> <?php echo "Rhapsody Residences, Muntinlupa City" ?> </p>
                 <p> <?php echo "Hobbies: Watching anime and playing guitar"; ?> </p>
-                <a href="mailto:marcelino.gem23@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto:marcelino.gem23@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -180,7 +161,7 @@
         </div>
         <div id="cedeno-profile" class="profile">
             <div class="profile-header">
-                <img src="images/cedeno.jpg" alt="Mark Bryan Cedeno">
+                <img src="../images/cedeno.jpg" alt="Mark Bryan Cedeno">
                 <h2>
                     <?php echo $cedeno ?>
                 </h2>
@@ -191,7 +172,7 @@
                 <p> <?php echo "24 years old"; ?> </p>
                 <p> <?php echo "Purok 4 Extension Alabang, Muntinlupa City"; ?> </p>
                 <p> <?php echo "Hobbies: Playing games, reading and watching yujitube"; ?> </p>
-                <a href="mailto: cedenomarkbryan04@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto: cedenomarkbryan04@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -199,7 +180,7 @@
         </div>
         <div id="rivas-profile" class="profile">
             <div class="profile-header">
-                <img src="images/rivas.jpg" alt="Jose Emmanuel Rivas">
+                <img src="../images/rivas.jpg" alt="Jose Emmanuel Rivas">
                 <h2>
                     <?php echo $rivas ?>
                 </h2>
@@ -210,7 +191,7 @@
                 <p> <?php echo "20 years old"; ?> </p>
                 <p> <?php echo "Talon IV, Las piñas City"; ?> </p>
                 <p> <?php echo "Hobbies: Playing guitar, tekken and mukbangers"; ?> </p>
-                <a href="mailto: rivasjoseemmanuel055@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto: rivasjoseemmanuel055@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -218,7 +199,7 @@
         </div>
         <div id="bunyi-profile" class="profile">
             <div class="profile-header">
-                <img src="images/bunyi.jpg" alt="John Michael Bunyi">
+                <img src="../images/bunyi.jpg" alt="John Michael Bunyi">
                 <h2>
                     <?php echo $bunyi ?>
                 </h2>
@@ -229,7 +210,7 @@
                 <p> <?php echo "21 years old"; ?> </p> 
                 <p> <?php echo "3 Cupang, Muntinlupa City"; ?> </p>
                 <p> <?php echo "Hobbies: Playing basketball, riding bike, and foodtrip"; ?> </p>
-                <a href="mailto: johnmichaelbunyi2@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto: johnmichaelbunyi2@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -237,7 +218,7 @@
         </div>
         <div id="falcon-profile" class="profile">
             <div class="profile-header">
-                <img src="images/falcon.jpg" alt="Frederick Falcon">
+                <img src="../images/falcon.jpg" alt="Frederick Falcon">
                 <h2>
                     <?php echo $falcon ?>
                 </h2>
@@ -248,7 +229,7 @@
                 <p> <?php echo "25 years old"; ?> </p>
                 <p> <?php echo "Purok 3 Harmony Ville Cupang, Muntinlupa City"; ?> </p>
                 <p> <?php echo "Hobbies: Watching Viva Mix"; ?> </p>
-                <a href="mailto: frederickfalcon2@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto: frederickfalcon2@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -256,7 +237,7 @@
         </div>
         <div id="alfaro-profile" class="profile">
             <div class="profile-header">
-                <img src="images/alfaro.jpg" alt="Edwil Mark Alfaro">
+                <img src="../images/alfaro.jpg" alt="Edwil Mark Alfaro">
                 <h2>
                     <?php echo $alfaro ?>
                 </h2>
@@ -267,7 +248,7 @@
                 <p>22 years old</p>
                 <p>BLK 1 Purok 1 Dolleton St. Bayanan Muntinlupa City</p>
                 <p>Hobbies: Playing Basketball</p>
-                <a href="mailto: alfaroedwil23@gmail.com" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto: alfaroedwil23@gmail.com" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
@@ -275,7 +256,7 @@
         </div>
         <div id="user-profile" class="profile">
             <div class="profile-header">
-                <img src="images\Boss.jpg" alt="User Profile">
+                <img src="../images/Boss.jpg" alt="User Profile">
                 <h2>
                     <?php echo $newuser ?>
                 </h2>
@@ -286,7 +267,7 @@
                 <p> <?php echo $age ?> </p>
                 <p> <?php echo $address ?> </p>
                 <p> <?php echo $hobbies ?> </p>
-                <a href="mailto:" class="icon-button"><img src="images/gmail.jfif"></a>
+                <a href="mailto:" class="icon-button"><img src="../images/gmail.jfif"></a>
                 <div>
                     <a href="#team-section" class="back-button">Back to Team</a>
                 </div>
